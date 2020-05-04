8588383cabaa69d40be1624ffc7ea9c19ffc02c2
Pavel1krp <pavel07007@gmail.com>
2020-05-05 00:36:56 +0300

    коммит

--- calc.js
+++ calc.js
@@ -1,45 +1,49 @@
+const priority = { '-': 1, '+': 1, '*': 2, '/': 2 };
+const methos = { '-': (a, b) => a - b, '+': (a, b) => a + b, '/': (a, b) => a / b, '*': (a, b) => a * b }
+
+const getRes = (a, b, key) => methos[key](a, b);
+
+const applySymbol = (stackS, stackN) => {
+    const res = getRes(stackN[stackN.length - 2], stackN[stackN.length - 1], stackS[stackS.length - 1]);
+    stackS.pop();
+    stackN.pop();
+    stackN.pop();
+    return res;
+};
+
+function isSymbols(lat) {
+    return Object.keys(priority).includes(lat);
+}
+
 function calculator(str) {
-    let ArrSum = [];
-    let result = 0;
     let StakSymbols = [];
     let StakNumbers = [];
-    let priority = { '-': 1, '+': 1, '*': 2, '/': 2 };
-    let methos = { '-': (a, b) => a - b, '+': (a, b) => a + b, '/': (a, b) => a / b, '*': (a, b) => a * b }
     for (let i = 0; i < str.length; i++) {
 
-        if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
-            if (StakSymbols.length == 0 || priority[str[i]] >= priority[StakSymbols[StakSymbols.length - 1]]) {
+        if (isSymbols(str[i])) {
+
+            const stakSL = StakSymbols.length;
+
+            if (stakSL == 0 || priority[str[i]] >= priority[StakSymbols[stakSL - 1]]) {
                 StakSymbols.push(str[i]);
-            }
-            else {
+            } else {
+
                 do {
-                    let a = methos[StakSymbols[StakSymbols.length - 1]](StakNumbers[StakNumbers.length - 2], StakNumbers[StakNumbers.length - 1]);
-                    StakNumbers.pop();
-                    StakNumbers.pop();
-                    StakSymbols.pop();
-                    StakNumbers.push(a);
+                    StakNumbers.push(applySymbol(StakSymbols, StakNumbers));
 
                 } while (priority[str[i]] === priority[StakSymbols[StakSymbols.length - 1]])
+
                 StakSymbols.push(str[i]);
             }
-
-        }
-        if (str[i] != '+' && str[i] != '-' && str[i] != '/' && str[i] != '*') {
+        } else {
             StakNumbers.push(parseInt(str[i]));
         }
     }
-    for (let i = StakNumbers.length - 1; i > 0; i--) {
-        // if (StakSymbols[i] == 0) {
-        //     continue
-        // }
-        let a = (methos[StakSymbols[i - 1]](StakNumbers[i - 1], StakNumbers[i]));
-        StakNumbers.pop();
-        StakNumbers.pop();
-        StakNumbers.push(a);
 
+    while(StakNumbers.length !== 1) {
+        StakNumbers.push(applySymbol(StakSymbols,StakNumbers));
     }
 
     return StakNumbers[0];
 }
-console.log(calculator('8-2*7-3*5'))
-
+console.log(calculator('8+2*7-3*5-2+3'))
